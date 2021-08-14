4592aff115dc55ed9a9ed217d487e60d78cfbc60
super1114 <super1114dev@gmail.com>
2021-08-14 14:14:23 -0400

    first commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+module.exports = {
+  entry: [
+    "./app/app.jsx"
+  ],
+  resolveLoader: {
+    modules: ['node_modules'],
+    moduleExtensions: ["-loader"]
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  output: {
+    /* global __dirname */
+    path: path.join(__dirname, "app"),
+    filename: "app.js"
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.less$/, loader: "style!css!less" },
+      { test: /\.jsx$/, loader: "react-hot" },
+      { test: /\.js$/, include: /app/, loader: "babel", query: { presets: ["es2015"] }},
+      { test: /\.jsx$/, include: /app/, loader: "babel", query: { presets: ["react", "es2015"] }},
+      { test: /\.json$/, loader: "json" },
+      { test: /\.woff|woff2$/, loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf$/, loader: "file" },
+      { test: /\.eot$/, loader: "file" },
+      { test: /\.svg$/, loader: "file" },
+      { test: /\.gif/, loader: 'url?limit=10000&mimetype=image/gif' },
+      { test: /\.jpg/, loader: 'url?limit=10000&mimetype=image/jpg' },
+      { test: /\.png/, loader: 'url?limit=10000&mimetype=image/png' }
+    ]
+  }
+};
